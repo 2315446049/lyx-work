46907760513ceafe0f0c4e4d647725f474aabd36
2315446049 <55129720+2315446049@users.noreply.github.com>
2019-09-25 21:34:06 +0800

    Delete index.js

--- lyx-work09.24/js/index.js
+++
@@ -1,9 +0,0 @@
- window.onscroll=function(){
-        var topScroll =document.documentElement.scrollTop;
-        var bignav = document.getElementById("appear");
-        if(topScroll <=245){
-           bignav.style.display ='none';
-        }else if(topScroll >245){ 
-		   bignav.style.display = 'block';
-        }
-    }
